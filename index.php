b71ae31865bbec0542a81fbbf690d27decfb70dc
dsabia <dsabia@hotmail.it>
2017-07-10 11:58:56 +0100

    Php init locale
    from https://stackoverflow.com/questions/17803702/auto-detect-language-and-redirect-user

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+$lc = ""; 
+
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
+    $lc = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+if($lc == "fr"){
+    header("location: /index_fr.html");
+    exit();
+} else if($lc == "it"){
+    header("location: /index.html");
+    exit();
+}
+else{ 
+    header("location: /index_en.html");
+    exit();
+}
+?>
